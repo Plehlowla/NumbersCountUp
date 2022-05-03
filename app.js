d1573e52c918bdbe7ca71ab7339f70bbb733fe9c
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-05-03 08:31:24 -0500

    selected span, and forEach

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('numbers project');
+const numbers = [...document.querySelectorAll('.number')];
+// console.log(numbers);
+
+
+// for(const number of numbers){
+//     console.log(number);
+// }
+
+const updateCount = (el) => {
+    console.log(el);
+}
+
+numbers.forEach((number) => {
+    // console.log(number);
+    updateCount(number);
+});
